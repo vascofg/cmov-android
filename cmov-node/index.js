a8807c1aab4899600fd7144fffe72ae0fc577df4
wso277 <oliveira.wilson93@gmail.com>
2015-11-08 13:22:13 +0000

    Added port

--- cmov-node/index.js
+++ cmov-node/index.js
@@ -4,7 +4,7 @@ var manifest = {
     server: {},
     connections: [
         {
-            port: 3000,
+            port: process.env.PORT || 3000,
             host: 'localhost'
         }
     ],
